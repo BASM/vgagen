8fc3f3a7bfd3e3a301d22d7f3503de56cadc1386
Leonid Myravjev <asm@asm.pp.ru>
2013-05-09 19:09:59 +0400

    Color schame

--- firmware/main.c
+++ firmware/main.c
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 //#include <avr/signal.h>
+#include <avr/sleep.h>
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
 #include <avr/wdt.h>
@@ -10,13 +11,15 @@
 #include <util/delay.h>
 //#include "symbol_tbl.h"
 
-#if 0
+#if 1
 #define vsync() {PORTB&=~1; _delay_us(4); PORTB|=1;}
 #define hsync() {PORTB&=~2; _delay_us(4); PORTB|=2;}
 #else
 #define vsync() {PORTB|=1; _delay_us(64); PORTB&=~1;}
 #define hsync() {PORTB|=2; _delay_us(4); PORTB&=~2;}
 #endif
+
+const char byte[2] PROGMEM={0x55,0xee};
 
 int line=0;
 
@@ -27,6 +30,7 @@ ISR(TIMER1_COMPA_vect)
   line++;
   if (line<3) {
     PORTB|=1;
+    PORTC=0;
   //  return;
   } else {
     PORTB&=~1;
@@ -34,61 +38,56 @@ ISR(TIMER1_COMPA_vect)
 
   hsync();
 
-  if ( (line>50) && (line<500) ){
-    _delay_us(2);//border
-
-    PORTB|=(1<<2);
-    _delay_us(6);
-    PORTB&=~(1<<2);
-
-    PORTB&=~(1<<3);
-    PORTB|=(1<<3);
+  char b;
+  if (line%2) {
+    b=byte[0];
+    b=byte[1];
   }
 
- // int i;
- // hsync();
- // _delay_us(4);
+  int i=0;
+  if ( (line>40) && (line<520) ){
+    _delay_us(2);//border
 
- // if ((i>100)&&(i<200))   PORTB|=(1<<2);
-//  _delay_us(20);
- // if ((i>100)&&(i<200))   PORTB&=~(1<<2);
-  //if ((i>22)&&(i<502))   PORTB&=~(1<<2);
-
-  //_delay_us(3);
+    int k;
+    for (k=0; k<16; k++) {
+      PORTB|=(1<<2);
+      _delay_loop_1(10);
+      //_delay_us(1);//border
+      //PORTB&=~(1<<2);
+      PORTC++;
+    }
+  }
+  //_delay_loop_1(4);
+  PORTB&=~(1<<2);
 }
-//         */
 
 int main(void)
 {
 
    wdt_disable();
    cli();
- 
+
   TCCR1A=(1<<WGM11) | (1<<WGM10);
   TCCR1B=(1<<WGM13) | (1<<WGM12) | (1<<CS10);//CS == 0
   OCR1A=0x31b;
   TIMSK1=(1<<OCIE1A);
   power_timer1_enable();
-              /*
-               *
-  TCCR0 = 0x00; //stop
-  TCNT0 = 0xC3; //set count, One VGA line 31.77us
-  TCCR0 = 1<<CS01; //start timer with prescaler select 1/8
-  TIMSK = 1<<TOV0; //enable interrupt from Timer0 overflow
-  // */
+
   sei();
 
-   _delay_ms(1000);
+  DDRB=~0;
+  PORTB=1;
 
-    DDRB=~0;
-    PORTB=1;
+  DDRC=~0;
+  PORTC=0;//=0xff;
+  while(1) {};
+/*
+  set_sleep_mode(SLEEP_MODE_IDLE); 	
 
-    while (1) {
-      //PORTB+=1;
-      _delay_ms(1);
-      //vsync();
-    }
+  while (1) {
+    sleep_mode();
+  }*/
 
 
-    return(0);
+  return(0);
 }
